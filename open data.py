7c3f115279d66b4395b666400feb3f03fd34f9ba
Adriel.ak.ak <adriel.ak.ak@gmail.com>
2023-05-16 21:00:50 +0200

    change

--- open data.py
+++ open data.py
@@ -1,7 +1,7 @@
 import csv
 import matplotlib.pyplot as plt
 
-# Initialiser les variables pour chaque colonne
+# Initialiser les variables pour chaque colonne pour pouvoir les incrémenter 
 nb_vote_blanc = 0
 nb_vote_nul = 0
 nb_exprime = 0
@@ -17,6 +17,8 @@ jadot_yannick = 0
 pecresse_valerie = 0
 poutou_philippe = 0
 dupont_aignan_nicolas = 0
+
+
 
 # Ouvrir le fichier CSV en mode lecture
 with open("csv/election.csv", "r") as fichier_csv:
@@ -42,15 +44,9 @@ with open("csv/election.csv", "r") as fichier_csv:
         poutou_philippe += float(ligne["poutou_philippe"])
         dupont_aignan_nicolas += float(ligne["dupont_aignan_nicolas"])
 
-#variable nécessaire au fonctionnement des descripteurs
-score = ""
-candidat = ""
 
 
-
-
-
-# Créer un dictionnaire avec les données
+# Créer un dictionnaire avec les données "key" :  value,
 donnees = {
     "nb_vote_blanc": nb_vote_blanc,
     "nb_vote_nul": nb_vote_nul,
@@ -67,8 +63,6 @@ donnees = {
     "pecresse_valerie": pecresse_valerie,
     "poutou_philippe": poutou_philippe,
     "dupont_aignan_nicolas": dupont_aignan_nicolas,
-    "score" : "",
-    "candidat" : ""
 
 }
 
@@ -77,15 +71,19 @@ fichier_csv = "csv/donnees_votes.csv"
 
 # Écrire les données dans le fichier CSV
 with open(fichier_csv, "w", newline="") as csvfile:
+    # Crée un objet writer avec un en-tête correspondant aux clés du dictionnaire "donnees"
     writer = csv.DictWriter(csvfile, fieldnames=donnees.keys())
+     # Écrit l'en-tête dans le fichier CSV
     writer.writeheader()
+    # Écrit une ligne contenant les données dans le fichier CSV
+    # "donnees" est un dictionnaire contenant les données à écrire
     writer.writerow(donnees)
 
 # Afficher un message de confirmation
 print("Les données ont été enregistrées dans le fichier", fichier_csv)
 
 
-# Afficher le résultat pour chaque colonne
+# Afficher le résultat pour chaque colonne (vérification)
 print("Nombre de votes blancs :", nb_vote_blanc)
 print("Nombre de votes nuls :", nb_vote_nul)
 print("Nombre de votes exprimés :", nb_exprime)
@@ -102,10 +100,7 @@ print("Nombre de votes pour Valerie Pecresse :", pecresse_valerie)
 print("Nombre de votes pour Philippe Poutou :", poutou_philippe)
 print("Nombre de votes pour Nicolas Dupont Aignan :", dupont_aignan_nicolas)
 
-
-
-
-
+#calcule le % pour chaque candidat avec round (2 chiffres après la ,)
 netvotemanu = round((macron_emmanuel / nb_exprime) * 100, 2)
 netvotemarine  = round((le_pen_marine / nb_exprime) * 100, 2)
 netvotemelanchon = round((melenchon_jean_luc / nb_exprime) * 100, 2)
@@ -120,9 +115,7 @@ netvotepoutou  = round((poutou_philippe / nb_exprime) * 100, 2)
 netvotevalerie  = round((pecresse_valerie/ nb_exprime) * 100, 2)
 netvoteroussel = round((roussel_fabien / nb_exprime) * 100, 2)
 
-
-#Partie graphique
-
+#Partie graphique (un exemple commenter on fera la même chose pour les 11 autres candidats)
 # Données
 labels = ['Emmanuel Macron', 'Autres']
 pourcentages = [netvotemanu, 100-netvotemanu]
@@ -239,10 +232,11 @@ fig.savefig("img/Figure12.png")
 print("Graphique enregistré sous le nom de Figure12.png")
 
 
-#Partie interraction avec l'utilisateur
+#Partie interraction avec l'utilisateur via le terminal
 
 candidat = int(input( "\n 1 : Emmanuel Macron \n 2 : Nathalie Arthaud \n 3 : Fabien Roussel\n 4 : Jean Lasalle\n 5 : Marine Le Pen\n 6 : Eric Zemmour\n 7 : Jean-Luc-Mélanchon\n 8 : Anne Hidalgo\n 9 : Yannick Jadot\n 10: Valérie Pécresse\n 11: Philippe Poutou\n 12: Nicolas Dupont Aignan \n Quel candidat voulez-vous choisir [1:12]:" ))
 
+#créer un dictionnaire pour avoir tous les cas de figures
 lut = {1: ("macron_emmanuel", "Emmanuel Macron"),
        2: ("arthaud_nathalie", "Nathalie Arthaud"),
        3: ("roussel_fabien", "Fabien Roussel"),
@@ -256,17 +250,19 @@ lut = {1: ("macron_emmanuel", "Emmanuel Macron"),
        11: ("poutou_philippe", "Philippe Poutou"),
        12: ("dupont_aignan_nicolas", "Nicolas Dupont-Aignan")}
 
+#regarder à l'index 0 et 1 pour la key et la value
 candidat_key = lut[candidat][0]
 candidat_name = lut[candidat][1]
 
 pourcentage_score = round((donnees[candidat_key] / nb_exprime) * 100, 2)
 
-fichier_csv = "csv/resultat.csv"
+fichier_csv = "csv/resultat.csv"    #générer un fichier csv pour chaque candidat choisi
 with open(fichier_csv, "w", newline="") as csvfile:
     writer = csv.writer(csvfile, delimiter=';', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-    writer.writerow(["candidat", "score_en_pourcentages", "nb_voies"])
+    writer.writerow(["candidat", "score_en_pourcentages", "nb_voies"]) #création de descripteur
     writer.writerow([candidat_name, pourcentage_score, donnees[candidat_key]])
 
+#afficher la phrase de conclusion en utilisant le formatage (f)
 print(f"{candidat_name} a obtenu {pourcentage_score}% des votes du premier tour \n Le fichier resultat.csv a été généré sur le candidat choisi")
 
 
